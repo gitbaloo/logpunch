049a497aeee5437bd347f3fedaaa53f3a8e42bdd
Nikolaj Vølver <nikolajvolver@outlook.com>
2024-07-18 17:51:10 +0200

    clientcontroller got same identity check as other controllers

--- backend/src/Server/Controllers/ClientController.cs
+++ backend/src/Server/Controllers/ClientController.cs
@@ -2,6 +2,7 @@ using Domain;
 using Infrastructure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Service.Login;
 using Shared;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -14,16 +15,37 @@ namespace Logpunch.Controllers
     public class ClientController : ControllerBase
     {
         private readonly IClientService _clientService;
+        private readonly ILoginService _loginService;
 
-        public ClientController(IClientService clientService)
+        public ClientController(IClientService clientService, ILoginService loginService)
         {
             _clientService = clientService;
+            _loginService = loginService;
         }
 
         [HttpGet("{employeeId}")]
-        public async Task<ActionResult<List<LogpunchClientDto>>> GetClients(Guid employeeId)
+        public async Task<ActionResult<List<LogpunchClientDto>>> GetClients(Guid? employeeId)
         {
-            var clients = await _clientService.GetClients(employeeId);
+            var token = HttpContext.Request.Headers.Authorization.ToString().Replace("Bearer ", "");
+            var user = await _loginService.ValidateToken(token);
+            Guid nonNullableEmployeeId;
+
+            if (!employeeId.HasValue)
+            {
+                nonNullableEmployeeId = user.Id;
+            }
+            else
+            {
+                nonNullableEmployeeId = employeeId.Value;
+            }
+
+            var clients = await _clientService.GetClients(nonNullableEmployeeId);
+
+            if (clients.Count == 0)
+            {
+                return NotFound(new { Message = "No clients could be found for this employee" });
+            }
+
             return Ok(clients);
         }
     }
